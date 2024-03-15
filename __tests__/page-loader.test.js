fdd62657140713a2bce6afa9e48c9a182bfd1aa5
Tema <artemiyd90@gmail.com>
2024-03-15 22:30:18 +0100

    Added ability to download pictures and replce src parameter in html for local src

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -12,10 +12,28 @@ describe('page-loader', () => {
     });
 
     test('access data from url', async () => {
-        const expectedHtml = '<html><body>Hello World!</body></html>';
+        const expectedHtml = '<html><head></head><body>Hello World!</body></html>';
         const scope = nock('https://ru.hexlet.io')
             .get('/')
             .reply(200, expectedHtml);
+
+        await pageLoader('https://ru.hexlet.io', tempDir);
+        try {
+            const fileContent = await fs.readFile(path.join(tempDir, 'ru-hexlet-io.html'), 'utf-8');
+            expect(fileContent).toBe(expectedHtml);
+        } catch (error) {
+            expect(error).toBeNull();
+        }
+    });
+    test('pictures download and src update', async () => {
+        const originalHtml = await fs.readFile('./__fixtures__/hexletIOCourses.html', 'utf-8');
+        const expectedHtml = await fs.readFile('./__fixtures__/hexletIOCoursesLocal.html', 'utf-8');
+        const originalPicture = await fs.readFile('./__fixtures__/assets/professions/nodejs.png');
+        const scope = nock('https://ru.hexlet.io')
+            .get('/')
+            .reply(200, originalHtml)
+            .get('/assets/professions/nodejs.png')
+            .reply(200, originalPicture);
 
         await pageLoader('https://ru.hexlet.io', tempDir);
         try {
--- src/page-loader.js
+++ src/page-loader.js
@@ -2,10 +2,13 @@ import fs from 'fs';
 import axios from 'axios';
 import path from 'path';
 import os from 'os';
+import cheerio from 'cheerio';
 
-const pageLoader = (url, dir = os.homedir()) => {
+const pageLoader = (url, dir = path.join(os.tmpdir(), `page-loader-${Date.now().toString()}`)) => {
     const urlObject = new URL(url);
-    const fileName = path.join(dir, urlObject.host.replace(/\W/g, '-') + '.html');
+    const hostName = urlObject.hostname;
+    const filesDir = path.join(dir, hostName.replace(/\W/g, '-') + '_files');
+    const fileName = path.join(dir, hostName.replace(/\W/g, '-') + '.html');
 
     return fs.promises.access(dir, fs.constants.F_OK)
         .catch(err => {
@@ -15,28 +18,57 @@ const pageLoader = (url, dir = os.homedir()) => {
             throw err;
         })
         .then(() => {
+            return fs.promises.access(filesDir, fs.constants.F_OK)
+                .catch(err => {
+                    if (err.code === 'ENOENT') {
+                        return fs.promises.mkdir(filesDir, { recursive: true });
+                    }
+                    throw err;
+                });
+        })
+        .then(() => {
             return axios({
                 method: 'get',
                 url: url,
-                responseType: 'stream'
+                responseType: 'text'
             });
         })
         .then(response => {
-            return new Promise((resolve, reject) => {
-                const stream = response.data.pipe(fs.createWriteStream(fileName));
-                stream.on('finish', () => {
-                    console.log('Page downloaded successfully!');
-                    resolve();
-                });
-                stream.on('error', error => {
-                    console.error('Error writing file:', error);
-                    reject(error);
-                });
+            const html = response.data;
+            const $ = cheerio.load(html); // Load HTML content into Cheerio
+
+            const imageDownloads = [];
+            $('img').each((index, element) => { // Iterate over image elements
+                const imageUrl = new URL($(element).attr('src'), url); // Resolve relative URLs
+                const imageName = path.basename(imageUrl.pathname);
+                const imagePath = path.join(filesDir, imageName);
+                const relativeImagePath = path.relative(dir, imagePath);
+                $(element).attr('src', relativeImagePath); // Update image src attribute
+                imageDownloads.push(
+                    axios({
+                        method: 'get',
+                        url: imageUrl.href,
+                        responseType: 'stream'
+                    })
+                    .then(response => {
+                        const stream = response.data.pipe(fs.createWriteStream(imagePath));
+                        return new Promise((resolve, reject) => {
+                            stream.on('finish', resolve);
+                            stream.on('error', reject);
+                        });
+                    })
+                );
             });
+
+            return Promise.all(imageDownloads)
+                .then(() => {
+                    fs.writeFileSync(fileName, $.html()); // Save HTML content after images are downloaded
+                    console.log('Page downloaded successfully!');
+                });
         })
         .catch(error => {
             console.error('Error fetching page:', error);
-            throw error; 
+            throw error; // Re-throw the error to propagate it to the caller
         });
 };
 
